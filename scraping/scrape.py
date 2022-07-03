1b803f9ed9feede8a1d0e8a3a8c28e310ec97a5a
xsych123 <67662284+noahgarrett@users.noreply.github.com>
2022-07-03 10:42:53 -0400

    further integrated build scraping and runes

--- scraping/scrape.py
+++ scraping/scrape.py
@@ -99,13 +99,13 @@ async def scrape_op_gg_builds(client: supabase.Client):
             .eq('position', champ_pos) \
             .execute().data
 
-        # region Runes
-        rune_tree = soup.find('div', {'class': 'css-1nomhew e1o8f102'})
+        # region Primary Rune Tree
+        primary_rune_tree = soup.find('div', {'class': 'css-1nomhew e1o8f102'})
 
-        primary_tree_name = rune_tree.find('h5').text
-        primary_tree_img = rune_tree.find('img').attrs['src']
+        primary_tree_name = primary_rune_tree.find('h5').text
+        primary_tree_img = primary_rune_tree.find('img').attrs['src']
 
-        primary_rows: list = rune_tree.findAll('div', {'class': 'row'})
+        primary_rows: list = primary_rune_tree.findAll('div', {'class': 'row'})
 
         keystone_items: list = primary_rows[1].findAll('div', {'class': 'item'})
         keystone_name = ""
@@ -146,7 +146,116 @@ async def scrape_op_gg_builds(client: supabase.Client):
                 p_3_img = img_url.attrs['src']
                 p_3_name = img_url.attrs['alt']
                 break
+        # endregion
 
-        print(p_3_name, p_3_img)
+        # region Secondary Rune Tree
+        secondary_rune_tree = soup.findAll('div', {'class': 'css-1nomhew e1o8f102'})[1]
+
+        secondary_tree_name = secondary_rune_tree.find('h5').text
+        secondary_tree_img = secondary_rune_tree.find('img').attrs['src']
+
+        secondary_rows: list = secondary_rune_tree.findAll('div', {'class': 'row'})
+
+        secondary_1_items: list = secondary_rows[1].findAll('div', {'class': 'item'})
+        s_1_name = None
+        s_1_img = None
+        for item in secondary_1_items:
+            img_url = item.find('img')
+            if not len(img_url.attrs['src'].split('grayscale')) > 1:
+                s_1_img = img_url.attrs['src']
+                s_1_name = img_url.attrs['alt']
+                break
+
+        secondary_2_items: list = secondary_rows[2].findAll('div', {'class': 'item'})
+        s_2_name = None
+        s_2_img = None
+        for item in secondary_2_items:
+            img_url = item.find('img')
+            if not len(img_url.attrs['src'].split('grayscale')) > 1:
+                if s_1_name is None or s_1_img is None:
+                    s_1_img = img_url.attrs['src']
+                    s_1_name = img_url.attrs['alt']
+                else:
+                    s_2_img = img_url.attrs['src']
+                    s_2_name = img_url.attrs['alt']
+                break
+
+        secondary_3_items: list = secondary_rows[3].findAll('div', {'class': 'item'})
+        for item in secondary_3_items:
+            img_url = item.find('img')
+            if not len(img_url.attrs['src'].split('grayscale')) > 1:
+                if s_1_name is None or s_1_img is None:
+                    s_1_img = img_url.attrs['src']
+                    s_1_name = img_url.attrs['alt']
+                else:
+                    s_2_img = img_url.attrs['src']
+                    s_2_name = img_url.attrs['alt']
+                break
+        # endregion
+
+        # region 3 Minor stat selection
+        stat_tree = soup.find('div', {'class': 'css-1ooi192 e1gtrici3'})
+
+        stat_rows = stat_tree.findAll('div', {'class': 'row'})
+
+        stat_1_items = stat_rows[0].findAll('div')
+        stat_1_name = ""
+        stat_1_img = ""
+        for item in stat_1_items:
+            img_url = item.find('img')
+            if not len(img_url.attrs['src'].split('grayscale')) > 1:
+                stat_1_img = img_url.attrs['src']
+                stat_1_name = img_url.attrs['alt']
+
+        stat_2_items = stat_rows[1].findAll('div')
+        stat_2_name = ""
+        stat_2_img = ""
+        for item in stat_2_items:
+            img_url = item.find('img')
+            if not len(img_url.attrs['src'].split('grayscale')) > 1:
+                stat_2_img = img_url.attrs['src']
+                stat_2_name = img_url.attrs['alt']
+
+        stat_3_items = stat_rows[2].findAll('div')
+        stat_3_name = ""
+        stat_3_img = ""
+        for item in stat_3_items:
+            img_url = item.find('img')
+            if not len(img_url.attrs['src'].split('grayscale')) > 1:
+                stat_3_img = img_url.attrs['src']
+                stat_3_name = img_url.attrs['alt']
+        # endregion
+
+        # region Runes Pick/Win rate
+        rune_data = soup.find('div', {'class': 'css-zyi49u e1jxk9el3'}).findAll('li')[0]
+
+        runes_pick_rate = rune_data.find('span', {'class': 'pick-rate'}).text.split("%")[0]
+        runes_games = rune_data.find('span', {'class': 'pick-rate'}).text.split("%")[1].replace('  Games', '').replace(',', '')
+        runes_win_rate = rune_data.find('span', {'class': 'win-rate'}).text.replace('%', '')
+        # endregion
+
+        # region Starter Items
+        starter_table_row = soup.find('div', {'class': 'css-11msl8b e1f5dr6w2'}).find('table').find('tbody').findAll('tr')[0]
+
+        starter_1_name = starter_table_row.findAll('img')[0].attrs['alt']
+        starter_1_img = starter_table_row.findAll('img')[0].attrs['src']
+        starter_2_name = starter_table_row.findAll('img')[1].attrs['alt']
+        starter_2_img = starter_table_row.findAll('img')[1].attrs['src']
+
+        starter_pick_rate = starter_table_row.findAll('td')[1].find('strong').text.replace('%', '')
+        starter_games = starter_table_row.findAll('td')[1].find('small').text.replace(' Games', '').replace(',', '')
+        starter_win_rate = starter_table_row.findAll('td')[2].text.replace('%', '')
+        # endregion
+
+        # region Boots
+        boots_table_row = soup.find('div', {'class': 'css-11msl8b e1f5dr6w2'}).findAll('table')[1].find('tbody').findAll('tr')[0]
+
+        boots_name = boots_table_row.find('img').attrs['alt']
+        boots_img = boots_table_row.find('img').attrs['src']
+
+        boots_pick_rate = boots_table_row.findAll('td')[1].find('strong').text.replace('%', '')
+        boots_games = boots_table_row.findAll('td')[1].find('small').text.replace(' Games', '').replace(',', '')
+        boots_win_rate = boots_table_row.findAll('td')[2].text.replace('%', '')
+        print(boots_games, boots_win_rate)
         # endregion
         break
